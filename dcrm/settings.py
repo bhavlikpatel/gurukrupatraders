5fb5c3968c07f10b96b8bb05f97274a77084caa5
bhavlikpatelCB <bhavlik.patel@cerebulb.com>
2024-09-30 23:50:56 +0530

    changes

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['bhavlikpatel.pythonanywhere.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
