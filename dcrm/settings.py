08254b98af6b69ed5eaa17547c83920fccbd2475
bhavlikpatelCB <bhavlik.patel@cerebulb.com>
2024-09-30 23:39:15 +0530

    added host

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-jcxge@8cwms-4-$&qia^6p+^8-qwrsw7vey#0e6e326apg3mvo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.246.97','127.0.0.1']
+ALLOWED_HOSTS = ['bhavlikpatel.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
