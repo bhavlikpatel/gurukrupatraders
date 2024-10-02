b6c8ea2eba8b4ef2bb30ab20ad180c48bf955f4a
bhavlikpatelCB <bhavlik.patel@cerebulb.com>
2024-10-02 13:27:58 +0530

    Jassmin

--- website/models.py
+++ website/models.py
@@ -33,17 +33,3 @@ class Record(models.Model):
 
     def __str__(self):
         return f"{self.truck_no} {self.delivered_at}"
-
-# class Record(models.Model):
-# 	created_at = models.DateTimeField(auto_now_add=True)
-# 	first_name = models.CharField(max_length=50)
-# 	last_name =  models.CharField(max_length=50)
-# 	email =  models.CharField(max_length=100)
-# 	phone = models.CharField(max_length=15)
-# 	address =  models.CharField(max_length=100)
-# 	city =  models.CharField(max_length=50)
-# 	state =  models.CharField(max_length=50)
-# 	zipcode =  models.CharField(max_length=20)
-
-# 	def __str__(self):
-# 		return(f"{self.first_name} {self.last_name}")
